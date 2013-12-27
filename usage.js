c541c12efcc1414f0627c571203fd1c60297b083
ibprogrammin <daniel@seriousmonkey.ca>
2013-12-27 18:18:00 -0500

    Update usage.js

--- usage.js
+++ usage.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function ($) {
-    $('.nav-element').jHideAndShowNav({"count": 6, "transitionDuration": 1000, "moreText": "More >>"});
+    $('.nav-menu').jHideAndShowNav({"count": 6, "transitionDuration": 1000, "moreText": "More >>"});
 });
